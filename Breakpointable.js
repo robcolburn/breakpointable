b8e0df469424c21248aa864d156acd31c917b035
Rob Colburn <rob@robcolburn.com>
2015-04-23 15:55:21 -0700

    Initial Drop

---
+++ Breakpointable.js
@@ -0,0 +1,44 @@
+/*global window*/
+
+/**
+ * Mixin
+ *
+ * @requires {array} breakpoints
+ * breakpoints [
+ *   {name: 'mobile', width: 0},
+ *   {name: 'desktop', width: 990},
+ * ]
+ *
+ * @requires {function} onBreakpointChange
+ * onBreakpointChange(breakpointName) {
+ *   // Act on breakpoint
+ * }
+ *
+ */
+var Breakpointable = {
+  componentDidMount: function () {
+    this.state.breakpoint = this.getBreakpointAt(this.getWindowWidth());
+    window.addEventListener('resize', this.onWindowResize);
+  },
+  componentWillUnmount: function () {
+    window.removeEventListener('resize', this.onWindowResize);
+  },
+  onWindowResize: function () {
+    this.setState({breakpoint: this.getBreakpointAt(this.getWindowWidth())});
+  },
+  getWindowWidth: function () {
+    window.innerWidth || window.clientWidth || window.document.documentElement.clientWidth;
+  },
+  getBreakpointAt: function (maxWidth) {
+    var width = -1;
+    var name;
+    for (var i = this.breakpoints.length - 1; i >= 0; i--) {
+      if (this.breakpoints[i].width > width && this.breakpoints[i].width <= maxWidth) {
+        width = this.breakpoints[i].width;
+        name = this.breakpoints[i].name;
+      }
+    }
+    return name;
+  }
+};
+module.exports = Breakpointable;
